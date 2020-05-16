881291a3092adc262da6c5c33321a0657adb18b3
Nikolay Volodarets <volodarec.nick.14@gmail.com>
2020-05-16 15:21:39 +0300

    Update main.tf

--- terraform_infrastructure/main.tf
+++ terraform_infrastructure/main.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
-  subscription_id = $SUB_ID
-  client_id       = $CLIENT_ID
-  client_secret   = $CLIENT_SECRET
-  tenant_id       = $TENANT_ID
+  subscription_id = "$SUB_ID"
+  client_id       = "$CLIENT_ID"
+  client_secret   = "$CLIENT_SECRET"
+  tenant_id       = "$TENANT_ID"
   features {}
 }
 
