87a28d3d5c2482ca9b4f702e11d6cf3f51b4f9ec
Ihor Palianychka <gorok97@gmail.com>
2020-05-19 12:31:48 +0300

    rgkdghddhhg

--- terraform_infrastructure/modules/autoscale-test/autoscale.tf
+++ terraform_infrastructure/modules/autoscale-test/autoscale.tf
@@ -48,16 +48,16 @@ resource "azurerm_virtual_machine_scale_set" "vm_scale_set" {
   resource_group_name = var.group_name
 
   automatic_os_upgrade = false
-  upgrade_policy_mode  = "Rolling"
+  #upgrade_policy_mode  = "Rolling"
 
-  rolling_upgrade_policy {
-    max_batch_instance_percent              = 20
-    max_unhealthy_instance_percent          = 20
-    max_unhealthy_upgraded_instance_percent = 5
-    pause_time_between_batches              = "PT0S"
-  }
+  #rolling_upgrade_policy {
+  #  max_batch_instance_percent              = 20
+  #  max_unhealthy_instance_percent          = 20
+  #  max_unhealthy_upgraded_instance_percent = 5
+  #  pause_time_between_batches              = "PT0S"
+  #}
 
-  health_probe_id = azurerm_lb_probe.lb_probe.id
+  #health_probe_id = azurerm_lb_probe.lb_probe.id
 
   sku {
     name     = "Standard_D2s_v3"
