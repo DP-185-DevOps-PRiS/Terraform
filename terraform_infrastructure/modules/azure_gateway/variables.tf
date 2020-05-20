7c592a578f1c089fec511ab33ac9b49d23a39e15
Nikolay Volodarets <volodarec.nick.14@gmail.com>
2020-05-20 16:58:07 +0300

    Update variables.tf

--- terraform_infrastructure/modules/azure_gateway/variables.tf
+++ terraform_infrastructure/modules/azure_gateway/variables.tf
@@ -7,7 +7,7 @@ variable "gateway_subnet_id" {}
 variable "gcp_public_ip" {}
 
 variable "address_space_local_nw_gw" {
-  default = ["10.128.0.0/20", "172.22.208.0/24"]
+  default = ["10.128.0.0/20", "10.142.0.0/20", "172.22.208.0/24"]
 }
 
 variable "shared_secret_key" {
